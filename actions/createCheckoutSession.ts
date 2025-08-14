d9d4d7e8634a56e332559315a39dd3136647d3d4
MinhKhoa2209 <khoadm.23it@vku.udn.vn>
2025-08-14 23:28:06 +0700

    update url

--- actions/createCheckoutSession.ts
+++ actions/createCheckoutSession.ts
@@ -38,6 +38,11 @@ export async function createCheckoutSession(
         ? process.env.NEXT_PUBLIC_SITE_URL
         : process.env.NEXT_PUBLIC_BASE_URL;
 
+    // const baseUrl =
+    //   process.env.NODE_ENV === "production"
+    //     ? `https://${process.env.VERCEL_URL}`
+    //     : `${process.env.NEXT_PUBLIC_BASE_URL}`;
+
     const successUrl = `${baseUrl}/success?session_id={CHECKOUT_SESSION_ID}&orderNumber=${metadata.orderNumber}`;
 
     const cancelUrl = `${baseUrl}/basket`;
