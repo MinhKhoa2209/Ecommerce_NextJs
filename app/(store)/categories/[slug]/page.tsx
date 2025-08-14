7da11063860eb27af357b3b66932fcc785c7515f
MinhKhoa2209 <khoadm.23it@vku.udn.vn>
2025-08-14 22:55:15 +0700

    update category

--- app/(store)/categories/[slug]/page.tsx
+++ app/(store)/categories/[slug]/page.tsx
@@ -1,35 +1,34 @@
-import ProductsView from '@/components/ProductsView';
-import { getAllProducts } from '@/sanity/lib/products/getAllProducts';
-import { getProductsByCategory } from '@/sanity/lib/products/getProductByCategory';
-import React from 'react';
+import ProductsView from "@/components/ProductsView";
+import { getAllProducts } from "@/sanity/lib/products/getAllProducts";
+import { getProductsByCategory } from "@/sanity/lib/products/getProductByCategory";
+import React from "react";
 
-interface CategorPageProps {
+interface CategoryPageProps {
   params: { slug: string };
 }
 
-async function CategorPage({ params }: CategorPageProps) {
-  const { slug } = params;
+export default async function CategoryPage({ params }: CategoryPageProps) {
+  const slug = params.slug.toLowerCase();
 
   const products =
     slug === "products"
       ? await getAllProducts()
       : await getProductsByCategory(slug);
 
+  const title =
+    slug === "products"
+      ? "All Products"
+      : slug
+          .split("-")
+          .map((word) => word.charAt(0).toUpperCase() + word.slice(1))
+          .join(" ") + " Collection";
+
   return (
-    <div className="flex flex-col items-center justify-top min-h-screen p-10">
+    <div className="flex flex-col items-center min-h-screen p-10">
       <div className="bg-white p-8 rounded-lg border border-gray-200 shadow-md w-full max-w-5xl">
-        <h1 className="text-3xl font-bold mb-6 text-center">
-          {slug === "Products"
-            ? "All Products"
-            : slug
-                .split("-")
-                .map((word) => word.charAt(0).toUpperCase() + word.slice(1))
-                .join(" ") + " Collection"}
-        </h1>
+        <h1 className="text-3xl font-bold mb-6 text-center">{title}</h1>
         <ProductsView products={products} />
       </div>
     </div>
   );
 }
-
-export default CategorPage;
